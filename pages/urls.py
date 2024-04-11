88ace556a2577069df7e3650e02ce6a68ff62824
rozo <rozosilvasantiagoalberto147@gmail.com>
2024-04-11 14:08:09 -0500

    no sé que pasó aquí

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path 
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, contact
+from django.urls import path
+from pages.utils import ImageLocalStorage
+from .views import *
 
-urlpatterns = [ 
-    path("", HomePageView.as_view(), name='home'),
-    path("about/",AboutPageView.as_view(), name='about'),
-    path('products/', ProductIndexView.as_view(), name='index'), 
-    path('products/create', ProductCreateView.as_view(), name='form'), 
-    path('products/<str:id>', ProductShowView.as_view(), name='show'), 
-    path('contact/', contact.as_view(), name='contact')
-] 
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/product-created', ProductCreatedView.as_view(), name='product-created'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('image-not-di/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view(), name='imagenotdi_save'),
+]
